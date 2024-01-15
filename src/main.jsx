819d20602dd6fa0f23c218f73fad48542859a051
Ildrassil <schneiderjakob7@googlemail.com>
2024-01-15 16:26:55 +0100

    Bonus Challenge

--- src/main.jsx
+++ src/main.jsx
@@ -4,3 +4,15 @@ let wordArray =["hallo","du","was","wer","Wie gehts dir?","Paulaner","deineMutte
 console.log(wordArray.filter(value => value.length > 5));
 console.log(array.reduce((firstValue, currentValue)=> firstValue +currentValue));
 console.log(array.some(value => value > 10))
+
+const list = [15, 6, 3213, 9, 0, 12, 8464 , 1, 1264, 481, 186, 1031, 194];
+const sortedList = list.sort((previousValue,currentValue)=> currentValue-previousValue);
+console.log(sortedList);
+let updatedList = list.map(value => value*value);
+console.log(updatedList);
+updatedList.splice(0,4);
+updatedList.sort((previousValue, currentValue) => previousValue- currentValue).splice(0,2);
+console.log(updatedList);
+const division=updatedList.filter(value => value%4 !== 0);
+console.log(division);
+console.log(division.reduce((firstValue,currentValue) => firstValue + currentValue));
